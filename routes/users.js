9ac98668a7b3e5e1b59c874cdd33bf0d39ef3997
zhufengnodejs <zhufengnodejs@126.com>
2016-06-18 17:41:20 +0800

    用户注册

--- routes/users.js
+++ routes/users.js
@@ -6,9 +6,21 @@ router.get('/reg', function(req, res, next) {
   //这是相对路径，是相对于views的相对路径
   res.render('user/reg', { title: '注册' });
 });
+/**
+ * 1. 任意长度的输入会产生相同长度的输出。
+ * 2. 不能从输出的字符串还原来输入的内容
+ * 3. 不同的输入一定会产生不同的输出
+ */
+function md5(str){
+    // 先指定算法 然后指定输入 输出的时候指定编码
+    return require('crypto').createHash('md5').update(str).digest('hex');
+}
 //注册 得到请求体中的对象并保存到数据库
 router.post('/reg', function(req, res, next) {
    var user = req.body;//得到请求体
+   //创建一个entity并保存到数据库中
+  user.avatar = "https://secure.gravatar.com/avatar/"+md5(user.email)+"?s=48";
+    user.password = md5(user.password);
    new Model.User(user).save(function(err,doc){
      if(err){
         console.error(err);
