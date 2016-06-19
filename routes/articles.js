07f511aaadd161a0d21346d410647ec7f44a8191
zhufengnodejs <zhufengnodejs@126.com>
2016-06-19 15:19:55 +0800

    更新文章

--- routes/articles.js
+++ routes/articles.js
@@ -18,30 +18,46 @@ router.get('/list',function(req, res, next) {
 
 //发表博客
 router.get('/add',middle.checkLogin, function(req, res, next) {
-  res.render('article/add',{ title: '发表文章' });
+  res.render('article/add',{ title: '发表文章',article:{} });
 });
-
+//不管新增加还是修改，点击保存的时候都提交到此路由下面
 router.post('/add',middle.checkLogin, function(req, res, next) {
   var article = req.body;
-  //把session中的用户ID赋给文章的作者
-  article.user = req.session.user._id;
-  if((!article.title) || (!article.content) ){
-    req.flash('error','标题或正文不能为空');
-    req.flash('error','发表文章失败');
-    return res.redirect('back');
-  }
-  //创建文章的entity也就是实体并保存到数据库中
-  new Model.Article(article).save(function(err,doc){
-    if(err){
-      //传二个参数表示赋值，第一个参数是类型，第二个是信息
-      //它们实际是保存在session中，所以一旦写入，可以在下次请求中得到
+  console.log(article);
+  if(article._id){//如果提交过来的文章对象有ID的话则认为是修改
+    Model.Article.update({_id:article._id},
+        {$set:{title:article.title,content:article.content}},function(err,result){
+          if(err){
+            req.flash('error','更新文章失败');
+            res.redirect('back');//跳回上一个页面继续编辑
+          }else{
+            req.flash('success','更新文章成功');
+            res.redirect('/articles/detail/'+article._id);//跳转到详情页看更新后的结果
+          }
+        }
+    );
+  }else{//否则认为是新增加
+//把session中的用户ID赋给文章的作者
+    article.user = req.session.user._id;
+    if((!article.title) || (!article.content) ){
+      req.flash('error','标题或正文不能为空');
       req.flash('error','发表文章失败');
-      res.redirect('back');
-    }else{
-      req.flash('success','发表文章成功');
-      res.redirect('/');
+      return res.redirect('back');
     }
-  });
+    //创建文章的entity也就是实体并保存到数据库中
+    new Model.Article(article).save(function(err,doc){
+      if(err){
+        //传二个参数表示赋值，第一个参数是类型，第二个是信息
+        //它们实际是保存在session中，所以一旦写入，可以在下次请求中得到
+        req.flash('error','发表文章失败');
+        res.redirect('back');
+      }else{
+        req.flash('success','发表文章成功');
+        res.redirect('/');
+      }
+    });
+  }
+
 });
 
 // /articles/detail/xxxx
@@ -78,4 +94,10 @@ router.get('/delete/:_id',function(req,res){
   });
 });
 
+router.get('/edit/:_id',function(req,res){
+  var _id = req.params._id;
+  Model.Article.findById(_id,function(err,doc){
+    res.render('article/add',{title:'编辑文章',article:doc});
+  })
+})
 module.exports = router;
